60b445f181e3f238f7dd2339a119554e3e626b26
Priyansh <saxenapriyansh95@gmail.com>
2026-09-14 06:02:25 +0530

    added the initial csv reader script

--- src/analysis.py
+++ src/analysis.py
@@ -4,6 +4,14 @@ import plotly.graph_objects as go
 
 # importing the raw csv file and creating a df from it 
 
+while True:
+    user_file = input(f"Please enter the name of the file you'd like to run the Quantitative Analysis Pack on: ")
+
+    try:
+        df = pd.read_csv(user_file, index_col='date', parse_dates=True)
+    except FileNotFoundError:
+        print(f"\nFile not found. Please ensure that the file is in the folder where this script runs from and ensure that you get the name exactly right.\n")
+
 # calculating standard returns (concept to research: .pct_change())
 
 # calculating the log returns (concept to research: np.log() and .shift()) 
