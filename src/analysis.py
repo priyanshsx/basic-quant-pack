4be7a5935dbab166af3e34f79402408cfe7cc2ab
Priyansh <saxenapriyansh95@gmail.com>
2026-09-14 05:57:29 +0530

    added the initial structure

--- src/analysis.py
+++ src/analysis.py
@@ -1,4 +1,20 @@
 import pandas as pd 
 import numpy as np 
-import matplotlib.pyplot as plt 
+import plotly.graph_objects as go 
+
+# importing the raw csv file and creating a df from it 
+
+# calculating standard returns (concept to research: .pct_change())
+
+# calculating the log returns (concept to research: np.log() and .shift()) 
+
+# calculating the rolling volatility (concept to research: .rolling(), .std())
+
+# calculating the 20D and 50D SMA (concept to research: .mean())
+
+# calculating the cumulative return (concept to research: .cumprod())
+
+# calculating the max drawdown 
+
+# visualization engine (concept to research: plotly candlestick charts)
 
